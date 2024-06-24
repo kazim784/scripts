18fc968b7884ec66ccaedc742408c81e08519fd9
kazim784 <kazim784@gmail.com>
2024-06-24 10:10:56 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,27 +1,44 @@
 import serial
 import time
 
-# Open the serial port
-ser = serial.Serial('/dev/serial0', 1500000, timeout=1)
+try:
+    # Open the serial port
+    ser = serial.Serial('/dev/serial0', 1500000, timeout=1)
+    print("Serial port opened successfully")
+except serial.SerialException as e:
+    print("Error opening serial port:", e)
+    exit(1)
 
-# Wait for the vehicle to respond
 while True:
-    ser.write(b"sysid 1\n")  # Send a MAVLink message to request the system ID
-    response = ser.readline()
-    if response:
-        print("Received response:", response)
-        break
-    time.sleep(0.1)
+    try:
+        # Send a MAVLink message to request the system ID
+        ser.write(b"sysid 1\n")
+        print("Sent sysid message")
+        response = ser.readline()
+        if response:
+            print("Received response:", response)
+            break
+        time.sleep(0.1)
+    except serial.SerialException as e:
+        print("Error reading from serial port:", e)
+        time.sleep(1)
 
-# Send a heartbeat message to the vehicle
-ser.write(b"heartbeat 1 1 1 1\n")
+try:
+    # Send a heartbeat message to the vehicle
+    ser.write(b"heartbeat 1 1 1 1\n")
+    print("Sent heartbeat message")
+except serial.SerialException as e:
+    print("Error writing to serial port:", e)
 
-# Wait for the vehicle to respond with a heartbeat message
 while True:
-    response = ser.readline()
-    if response:
-        print("Received heartbeat response:", response)
-        break
-    time.sleep(0.1)
+    try:
+        response = ser.readline()
+        if response:
+            print("Received heartbeat response:", response)
+            break
+        time.sleep(0.1)
+    except serial.SerialException as e:
+        print("Error reading from serial port:", e)
+        time.sleep(1)
 
 print("Connected to vehicle!")
